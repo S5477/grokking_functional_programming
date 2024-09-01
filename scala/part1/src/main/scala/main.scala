4857372df3d8fe798834c833f54858f10e7663bc
viacheslavutrobin <viacheslav.utrobin@buddler.com>
2024-09-01 09:59:09 +0500

    p1ch2

--- scala/part1/src/main/scala/main.scala
+++ scala/part1/src/main/scala/main.scala
@@ -1,18 +1,18 @@
 
 @main
 def main(): Unit = {
-  val shoppingCart = ShoppingCart
-  val items = List("Book", "Shirt", "Pants")
-  val discount = shoppingCart.getDiscountPercentage(items)
-  println(s"Discount percentage: $discount%")
+    val shoppingCart = ShoppingCart
+    val items = List("Book", "Shirt", "Pants")
+    val discount = shoppingCart.getDiscountPercentage(items)
+    println(s"Discount percentage: $discount%")
 }
 
 object ShoppingCart {
-  def getDiscountPercentage(items: List[String]): Int = {
-    if (items.contains("Book")) {
-       5
-    } else {
-       0
+    def getDiscountPercentage(items: List[String]): Int = {
+        if (items.contains("Book")) {
+            5
+        } else {
+            0
+        }
     }
-  }
 }
