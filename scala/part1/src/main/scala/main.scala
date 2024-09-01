dd4a29adb636c118aaead8453cdd79deff42616b
viacheslavutrobin <viacheslav.utrobin@buddler.com>
2024-09-01 10:55:15 +0500

    coffee break ch2

--- scala/part1/src/main/scala/main.scala
+++ scala/part1/src/main/scala/main.scala
@@ -5,6 +5,18 @@ def main(): Unit = {
     val items = List("Book", "Shirt", "Pants")
     val discount = shoppingCart.getDiscountPercentage(items)
     println(s"Discount percentage: $discount%")
+
+    var names: List[String] = List.empty
+
+    println(TipCalculator.getTipPercentage(names))
+
+    names = List("Alice", "Bob", "Charlie")
+
+    println(TipCalculator.getTipPercentage(names))
+
+    names = names ::: List("David", "Eve", "Frank")
+
+    println(TipCalculator.getTipPercentage(names))
 }
 
 object ShoppingCart {
@@ -14,5 +26,13 @@ object ShoppingCart {
         } else {
             0
         }
+    }
+}
+
+object TipCalculator {
+    def getTipPercentage(names: List[String]): Int = {
+        if (names.length > 5) 20
+        else if(names.nonEmpty)  10
+        else 0
     }
 }
