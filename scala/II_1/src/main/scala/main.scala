5b93a2efede0d090aa00260a650cd1f82dfca11b
viacheslavutrobin <viacheslav.utrobin@buddler.com>
2024-10-02 21:52:24 +0500

    flatten

--- scala/II_1/src/main/scala/main.scala
+++ scala/II_1/src/main/scala/main.scala
@@ -1,6 +1,18 @@
 
 @main
 def main(): Unit = {
-  println("Hello world!")
+  val books = List(
+    Book("FP in Scala", List("Chiusano", "Bjarnason")),
+    Book("The Hobbit", List("Tolkien"))
+  )
+
+  println(books.map(_.authors).flatten)
 }
+
+case class  Book(title: String, authors: List[String])
+
+
+
+
+
 
